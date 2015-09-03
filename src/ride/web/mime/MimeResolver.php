b4531bb1561a6def4980e1bd6bb05771c34eb500
Kaya <kayalion@kayalion.net>
2015-09-03 19:21:16 +0200

    extended mime resolver

--- src/ride/web/mime/MimeResolver.php
+++ src/ride/web/mime/MimeResolver.php
@@ -14,4 +14,11 @@ interface MimeResolver {
      */
     public function getMimeTypeByExtension($extension);
 
+    /**
+     * Gets the file extension for files with the provided MIME type
+     * @param string $mimeType MIME type
+     * @return string|boolean File extension of the MIME type, false otherwise
+     */
+    public function getExtensionForMimeType($mimeType);
+
 }
